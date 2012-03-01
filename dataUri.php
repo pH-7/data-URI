34e3778d868574d87b790140bde97420ee7110d1
Pierre <ph_7@hotmail.be>
2012-03-01 03:14:48 +0100

    Adding a comment

--- dataUri.php
+++ dataUri.php
@@ -9,7 +9,7 @@
  * @version     $Id: dataUri.php 2012-01-12 pierrehs $
  */
  
-   /**
+  /**
    * @desc Data URI Function base64
    * @param string $sFile
    * @return string Returns format: data:[<MIME-type>][;base64],<data>
